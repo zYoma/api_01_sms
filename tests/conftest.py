96552eb0d52856295672eafba5676779ec4da7f7
zYoma <56995243+zYoma@users.noreply.github.com>
2020-03-16 15:37:32 +0300

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from os.path import abspath
+from os.path import dirname
+
+root_dir = dirname(dirname(abspath(__file__)))
+sys.path.append(root_dir)
+
+os.environ['ACCOUNT_SID'] = ''
+os.environ['AUTH_TOKEN'] = ''
+os.environ['VK_TOKEN'] = ''
+
+pytest_plugins = [
+    'tests.fixtures.fixture_twilio',
+    'tests.fixtures.fixture_vk',
+]
